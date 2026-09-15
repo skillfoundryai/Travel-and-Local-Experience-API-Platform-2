02bffec91d9080e98e3a98e4dd81f552a2ebc478
skillfoundryai <skillfoundryai@gmail.com>
2026-09-15 04:58:00 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 
 const app = express();
 const port = Number.parseInt(process.env.PORT ?? "3000", 10);
+const nodeenv =  process.env.NODE_ENV; 
 
 app.listen(port, () => {
-  console.log(`Wayfarinook API listening on port ${port}`);
+  //console.log(`Wayfarinook API listening on port ${port}`);
 });
