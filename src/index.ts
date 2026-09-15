2b7b01cb130df21c4780ddd89a8dc18b09ed107b
skillfoundryai <skillfoundryai@gmail.com>
2026-09-15 05:09:00 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ const app = express();
 const port = Number.parseInt(process.env.PORT ?? "3000", 10);
 const nodeenv =  process.env.NODE_ENV; 
 
-const isPortCorect !isNaN(port) && port> 999 && port <9999
+const isPortCorect = !isNaN(port) && port> 999 && port <9999
 const isCorrectNodeenv =  !!nodeenv && nodeenv.length>2
 if(!isPortCorect || !isCorrectNodeenv){
-//throw new Error
-  console.log("Error with PORT or Node_ENV")
+throw new Error("Error with PORT or Node_ENV");
+ 
 }
 
 app.listen(port, () => {
